f3db12c84f47c3528800fe0ec03ba413f0b73635
Jong jae Park <pjj186@naver.com>
2022-08-16 00:12:05 +0900

    Token Logic

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -5,17 +5,23 @@ import { NextApiRequest, NextApiResponse } from "next";
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { phone, email } = req.body;
   const payload = phone ? { phone: +phone } : { email: email };
-  const user = await client.user.upsert({
-    where: {
-      ...payload,
+  const token = await client.token.create({
+    data: {
+      payload: "123",
+      user: {
+        connectOrCreate: {
+          where: {
+            ...payload,
+          },
+          create: {
+            name: "Anonymous",
+            ...payload,
+          },
+        },
+      },
     },
-    create: {
-      name: "Anonymous",
-      ...payload,
-    },
-    update: {},
   });
-  console.log(user);
+  console.log(token);
   res.status(200).end();
 }
 
