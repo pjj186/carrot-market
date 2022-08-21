bbc0992e9f2a30cff15a9ce94a700abee8e5d2f7
Jong jae Park <pjj186@naver.com>
2022-08-22 01:28:51 +0900

    Post Detail

---
+++ pages/api/posts/[id].ts
@@ -0,0 +1,58 @@
+import client from "@libs/server/client";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    query: { id },
+  } = req;
+  const post = await client.post.findUnique({
+    where: {
+      id: +id?.toString()!,
+    },
+    include: {
+      user: {
+        select: {
+          id: true,
+          name: true,
+          avatar: true,
+        },
+      },
+      answers: {
+        select: {
+          answer: true,
+          id: true,
+          user: {
+            select: {
+              id: true,
+              name: true,
+              avatar: true,
+            },
+          },
+        },
+      },
+      _count: {
+        select: {
+          answers: true,
+          wonderings: true,
+        },
+      },
+    },
+  });
+  if (!post) res.status(404).json({ ok: false, error: "Not found post" });
+  res.json({
+    ok: true,
+    post,
+  });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ["GET"],
+    handler: handler,
+  })
+);
