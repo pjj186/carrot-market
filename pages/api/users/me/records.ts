47cd9fd833ef9fe2d3b350a638a0d186f175371d
Jong jae Park <pjj186@naver.com>
2022-09-04 20:11:58 +0900

    Add Records

---
+++ pages/api/users/me/records.ts
@@ -0,0 +1,29 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
+import { withApiSession } from "@libs/server/withSession";
+import { Kind } from "@prisma/client";
+
+const handler = async (
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) => {
+  const {
+    session: { user },
+    query: { kind },
+  } = req;
+  const kindStr = kind?.toString()!;
+  const records = await client.record.findMany({
+    where: {
+      userId: user?.id,
+      kind: kindStr as Kind,
+    },
+    include: { product: true },
+  });
+  res.json({
+    ok: true,
+    [kindStr]: records,
+  });
+};
+
+export default withApiSession(withHandler({ methods: ["GET"], handler }));
