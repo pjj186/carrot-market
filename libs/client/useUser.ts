17ddbf3c04f811145e56ca84e946bd801836c7e6
Jong jae Park <pjj186@naver.com>
2022-09-10 17:51:44 +0900

    Edit Profile

--- libs/client/useUser.ts
+++ libs/client/useUser.ts
@@ -1,13 +1,19 @@
+import { User } from "@prisma/client";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import useSWR from "swr";
+
+interface ProfileResponse {
+  ok: boolean;
+  profile: User;
+}
 
 interface IuseUser {
   isPublic: boolean;
 }
 
 export default function useUser({ isPublic = false }: IuseUser) {
-  const { data, error } = useSWR("/api/users/me");
+  const { data, error } = useSWR<ProfileResponse>("/api/users/me");
   const router = useRouter();
   useEffect(() => {
     if (data && !data.ok && !isPublic) {
--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -1,13 +1,42 @@
 import useUser from "@libs/client/useUser";
 import type { NextPage } from "next";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
 import Button from "../../components/button";
 import Input from "../../components/input";
 import Layout from "../../components/layout";
 
+interface EditProfileForm {
+  email?: string;
+  phone?: string;
+  formErrors?: string;
+}
+
 const EditProfile: NextPage = () => {
+  const { user } = useUser({ isPublic: false });
+  const {
+    register,
+    setValue,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<EditProfileForm>();
+  useEffect(() => {
+    if (user?.email) setValue("email", user.email);
+    if (user?.phone) setValue("phone", user.phone);
+  }, [user, setValue]);
+
+  const onValid = (form: EditProfileForm) => {
+    console.log(form);
+    if (form.email === "" && form.phone === "") {
+      setError("formErrors", {
+        message: "Email or PhoneNumber are required. You need choose one.",
+      });
+    }
+  };
   return (
     <Layout canGoBack title="Edit Profile">
-      <form className="py-10 px-4 space-y-4">
+      <form onSubmit={handleSubmit(onValid)} className="py-10 px-4 space-y-4">
         <div className="flex items-center space-x-3">
           <div className="w-14 h-14 rounded-full bg-slate-500" />
           <label
@@ -23,14 +52,26 @@ const EditProfile: NextPage = () => {
             />
           </label>
         </div>
-        <Input required label="Email address" name="email" type="email" />
         <Input
-          required
+          register={register("email")}
+          required={false}
+          label="Email address"
+          name="email"
+          type="email"
+        />
+        <Input
+          register={register("phone")}
+          required={false}
           label="Phone number"
           name="phone"
           type="number"
           kind="phone"
         />
+        {errors.formErrors ? (
+          <span className="mt-2 text-red-500 font-bold block">
+            {errors.formErrors.message}
+          </span>
+        ) : null}
         <Button text="Update profile" />
       </form>
     </Layout>
