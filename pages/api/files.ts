baccf0e5c5af1c1d70601d55b1d0577aef3cc2c2
JongJae Park <pjj186@naver.com>
2022-09-20 02:19:50 +0900

    Cloudflare Setup

---
+++ pages/api/files.ts
@@ -0,0 +1,21 @@
+import client from "@libs/server/client";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  res.json({
+    ok: true,
+    url: "",
+  });
+}
+
+export default withApiSession(
+  withHandler({
+    methods: ["POST"],
+    handler: handler,
+  })
+);
