195d8d0984a27a28054248e1d74d5e4e1ef84787
JongJae Park <pjj186@naver.com>
2022-10-07 01:06:42 +0900

    Streaming API

--- pages/api/streams/index.ts
+++ pages/api/streams/index.ts
@@ -17,8 +17,29 @@ async function handler(
   const backendPage = +page! - 1;
 
   if (req.method === "POST") {
+    const {
+      result: {
+        uid,
+        rtmps: { streamKey, url },
+      },
+    } = await (
+      await fetch(
+        `https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/stream/live_inputs`,
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${process.env.CLOUDFLARE_STREAM_TOKEN}`,
+          },
+          body: `{"meta": {"name":"${name}"},"recording": { "mode": "automatic", "timeoutSeconds": 10 }}`,
+        }
+      )
+    ).json();
+
     const stream = await client.stream.create({
       data: {
+        cloudflareId: uid,
+        cloudflareUrl: url,
+        cloudflareKey: streamKey,
         name,
         price,
         description,
